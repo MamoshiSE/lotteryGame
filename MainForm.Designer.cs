c3fb007f91d79f538fc93992dd1a41938031ac1b
MamoshiSE <33344169+MamoshiSE@users.noreply.github.com>
2020-01-23 22:10:07 +0100

    Initial push

---
+++ MainForm.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Graphics1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lotteryValue7 = new System.Windows.Forms.TextBox();
+            this.lotteryValue6 = new System.Windows.Forms.TextBox();
+            this.lotteryValue5 = new System.Windows.Forms.TextBox();
+            this.lotteryValue4 = new System.Windows.Forms.TextBox();
+            this.lotteryValue3 = new System.Windows.Forms.TextBox();
+            this.lotteryValue2 = new System.Windows.Forms.TextBox();
+            this.lotteryValue1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.correct7 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.correct6 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.correct5 = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.startLottery = new System.Windows.Forms.Button();
+            this.drawAmount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Din lottorad:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.lotteryValue7);
+            this.panel1.Controls.Add(this.lotteryValue6);
+            this.panel1.Controls.Add(this.lotteryValue5);
+            this.panel1.Controls.Add(this.lotteryValue4);
+            this.panel1.Controls.Add(this.lotteryValue3);
+            this.panel1.Controls.Add(this.lotteryValue2);
+            this.panel1.Controls.Add(this.lotteryValue1);
+            this.panel1.Location = new System.Drawing.Point(92, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(504, 57);
+            this.panel1.TabIndex = 1;
+            // 
+            // lotteryValue7
+            // 
+            this.lotteryValue7.Location = new System.Drawing.Point(406, 19);
+            this.lotteryValue7.Name = "lotteryValue7";
+            this.lotteryValue7.Size = new System.Drawing.Size(58, 20);
+            this.lotteryValue7.TabIndex = 6;
+            this.lotteryValue7.TextChanged += new System.EventHandler(this.lotteryValue7_TextChanged);
+            this.lotteryValue7.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.lotteryValue_KeyPress);
+            // 
+            // lotteryValue6
+            // 
+            this.lotteryValue6.Location = new System.Drawing.Point(342, 19);
+            this.lotteryValue6.Name = "lotteryValue6";
+            this.lotteryValue6.Size = new System.Drawing.Size(58, 20);
+            this.lotteryValue6.TabIndex = 5;
+            this.lotteryValue6.TextChanged += new System.EventHandler(this.lotteryValue6_TextChanged);
+            this.lotteryValue6.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.lotteryValue_KeyPress);
+            // 
+            // lotteryValue5
+            // 
+            this.lotteryValue5.Location = new System.Drawing.Point(278, 19);
+            this.lotteryValue5.Name = "lotteryValue5";
+            this.lotteryValue5.Size = new System.Drawing.Size(58, 20);
+            this.lotteryValue5.TabIndex = 4;
+            this.lotteryValue5.TextChanged += new System.EventHandler(this.lotteryValue5_TextChanged);
+            this.lotteryValue5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.lotteryValue_KeyPress);
+            // 
+            // lotteryValue4
+            // 
+            this.lotteryValue4.Location = new System.Drawing.Point(214, 19);
+            this.lotteryValue4.Name = "lotteryValue4";
+            this.lotteryValue4.Size = new System.Drawing.Size(58, 20);
+            this.lotteryValue4.TabIndex = 3;
+            this.lotteryValue4.TextChanged += new System.EventHandler(this.lotteryValue4_TextChanged);
+            this.lotteryValue4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.lotteryValue_KeyPress);
+            // 
+            // lotteryValue3
+            // 
+            this.lotteryValue3.Location = new System.Drawing.Point(150, 19);
+            this.lotteryValue3.Name = "lotteryValue3";
+            this.lotteryValue3.Size = new System.Drawing.Size(58, 20);
+            this.lotteryValue3.TabIndex = 2;
+            this.lotteryValue3.TextChanged += new System.EventHandler(this.lotteryValue3_TextChanged);
+            this.lotteryValue3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.lotteryValue_KeyPress);
+            // 
+            // lotteryValue2
+            // 
+            this.lotteryValue2.Location = new System.Drawing.Point(86, 19);
+            this.lotteryValue2.Name = "lotteryValue2";
+            this.lotteryValue2.Size = new System.Drawing.Size(58, 20);
+            this.lotteryValue2.TabIndex = 1;
+            this.lotteryValue2.TextChanged += new System.EventHandler(this.lotteryValue2_TextChanged);
+            this.lotteryValue2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.lotteryValue_KeyPress);
+            // 
+            // lotteryValue1
+            // 
+            this.lotteryValue1.Location = new System.Drawing.Point(22, 19);
+            this.lotteryValue1.Name = "lotteryValue1";
+            this.lotteryValue1.Size = new System.Drawing.Size(58, 20);
+            this.lotteryValue1.TabIndex = 0;
+            this.lotteryValue1.TextChanged += new System.EventHandler(this.lotteryValue1_TextChanged);
+            this.lotteryValue1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.lotteryValue_KeyPress);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Location = new System.Drawing.Point(-2, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(637, 263);
+            this.panel2.TabIndex = 8;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.correct7);
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Controls.Add(this.correct6);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Controls.Add(this.correct5);
+            this.panel4.Location = new System.Drawing.Point(120, 182);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(433, 50);
+            this.panel4.TabIndex = 9;
+            // 
+            // correct7
+            // 
+            this.correct7.Location = new System.Drawing.Point(340, 10);
+            this.correct7.Name = "correct7";
+            this.correct7.Size = new System.Drawing.Size(62, 20);
+            this.correct7.TabIndex = 13;
+            this.correct7.TextChanged += new System.EventHandler(this.correct7_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(288, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "7 rätt:";
+            // 
+            // correct6
+            // 
+            this.correct6.Location = new System.Drawing.Point(207, 10);
+            this.correct6.Name = "correct6";
+            this.correct6.Size = new System.Drawing.Size(62, 20);
+            this.correct6.TabIndex = 11;
+            this.correct6.TextChanged += new System.EventHandler(this.correct6_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(155, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "6 rätt:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "5 rätt:";
+            // 
+            // correct5
+            // 
+            this.correct5.Location = new System.Drawing.Point(69, 10);
+            this.correct5.Name = "correct5";
+            this.correct5.Size = new System.Drawing.Size(62, 20);
+            this.correct5.TabIndex = 2;
+            this.correct5.TextChanged += new System.EventHandler(this.correct5_TextChanged);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.startLottery);
+            this.panel3.Controls.Add(this.drawAmount);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(133, 91);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(408, 49);
+            this.panel3.TabIndex = 8;
+            // 
+            // startLottery
+            // 
+            this.startLottery.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.startLottery.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startLottery.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startLottery.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.startLottery.Location = new System.Drawing.Point(271, 11);
+            this.startLottery.Name = "startLottery";
+            this.startLottery.Size = new System.Drawing.Size(115, 29);
+            this.startLottery.TabIndex = 10;
+            this.startLottery.Text = "Starta lotto";
+            this.startLottery.UseVisualStyleBackColor = false;
+            this.startLottery.Click += new System.EventHandler(this.startLottery_Click_1);
+            // 
+            // drawAmount
+            // 
+            this.drawAmount.Location = new System.Drawing.Point(148, 16);
+            this.drawAmount.Name = "drawAmount";
+            this.drawAmount.Size = new System.Drawing.Size(117, 20);
+            this.drawAmount.TabIndex = 9;
+            this.drawAmount.TextChanged += new System.EventHandler(this.drawAmount_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Antal dragningar:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 257);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.Text = "Lottoprogram";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button startLottery;
+        private System.Windows.Forms.TextBox drawAmount;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox correct5;
+        private System.Windows.Forms.TextBox correct7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox correct6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox lotteryValue7;
+        private System.Windows.Forms.TextBox lotteryValue6;
+        private System.Windows.Forms.TextBox lotteryValue5;
+        private System.Windows.Forms.TextBox lotteryValue4;
+        private System.Windows.Forms.TextBox lotteryValue3;
+        private System.Windows.Forms.TextBox lotteryValue2;
+        private System.Windows.Forms.TextBox lotteryValue1;
+    }
+}
+
